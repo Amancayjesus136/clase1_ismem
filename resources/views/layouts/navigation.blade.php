50778b8644954ca3e5be81a742001888e6d80ae3
Amancayjesus136 <amancayjesus136@gmail.com>
2024-11-19 19:03:01 -0500

    clone

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,12 @@
                         {{ __('Clientes') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('pais.index')" :active="request()->routeIs('pais')">
+                        {{ __('Pais') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\PaisController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,5 +20,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('clientes', ClienteController::class);
+Route::resource('pais', PaisController::class);
+
 
 require __DIR__.'/auth.php';
